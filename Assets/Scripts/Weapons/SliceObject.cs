53723db3149bde22c47b6082d7b0511517ed380b
Anakaryna <78374876+Anakaryna@users.noreply.github.com>
2024-04-16 23:30:23 +0200

    Add Slicing logic

--- Assets/Scripts/Weapons/SliceObject.cs
+++ Assets/Scripts/Weapons/SliceObject.cs
@@ -1,9 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using EzySlice;
+
 
 public class SliceObject : MonoBehaviour
 {
+    public Transform startSlicePoint;
+    public Transform endSlicePoint;
+    public VelocityEstimator velocityEstimator;
+    public LayerMask sliceableLayer;
+
+    public Material crossSectionMaterial;
+
+    public float cutForce = 2000;
+        
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +20,51 @@ public class SliceObject : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        Debug.DrawLine(startSlicePoint.position, endSlicePoint.position, Color.red); // Add this line
+        bool hasHit = Physics.Linecast(startSlicePoint.position, endSlicePoint.position, out RaycastHit hit, sliceableLayer);
+        if (hasHit)
+        {
+            GameObject target = hit.transform.gameObject;
+            Slice(target);
+        } 
+    }
+
+    
+    // private void OnCollisionEnter(Collision collision)
+    // {
+    //     if (collision.gameObject.layer == sliceableLayer)
+    //     {
+    //         GameObject target = collision.gameObject;
+    //         Slice(target);
+    //     }
+    // }
+
+    public void Slice(GameObject target)
+    {
+        Vector3 velocity = velocityEstimator.GetVelocityEstimate();
+        Vector3 planeNormal = Vector3.Cross(endSlicePoint.position - startSlicePoint.position, velocity);
+        planeNormal.Normalize();
         
+        SlicedHull hull = target.Slice(endSlicePoint.position, planeNormal);
+
+        if (hull != null)
+        {
+            GameObject upperHull = hull.CreateUpperHull(target,crossSectionMaterial);
+            SetupSlicedComponent(upperHull);
+            GameObject lowerHull = hull.CreateLowerHull(target,crossSectionMaterial);
+            SetupSlicedComponent(lowerHull);
+            
+            Destroy(target);
+        }
+    }
+
+    public void SetupSlicedComponent(GameObject slicedObject)
+    {
+        Rigidbody rb = slicedObject.AddComponent<Rigidbody>();
+        MeshCollider collider = slicedObject.AddComponent<MeshCollider>();
+        collider.convex = true;
+        rb.AddExplosionForce(cutForce, slicedObject.transform.position, 1);
     }
 }

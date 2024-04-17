cd90534d26a572ac17472b32bec38a4ddadb7907
Frisk197 <bonnerondorian@gmail.com>
2024-04-17 21:48:20 +0200

    post merge fix

--- Assets/Scripts/switchScene.cs
+++ Assets/Scripts/switchScene.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class switchScene : MonoBehaviour
 {
-    public SceneAsset scene;
     private void OnTriggerEnter(Collider other)
     {
         print(other.gameObject);
-        SceneManager.LoadScene(scene.name);
+        SceneManager.LoadScene(0);
     }
 }

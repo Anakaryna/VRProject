179918b4b0462c4b0bea4f2871707815a3bdd0fa
Anakaryna <78374876+Anakaryna@users.noreply.github.com>
2024-04-17 10:25:34 +0200

    Add minVelocity to slice object

--- Assets/Scripts/Weapons/SliceObject.cs
+++ Assets/Scripts/Weapons/SliceObject.cs
@@ -1,53 +1,48 @@
 using UnityEngine;
 using EzySlice;
 
-
 public class SliceObject : MonoBehaviour
 {
     public Transform startSlicePoint;
     public Transform endSlicePoint;
     public VelocityEstimator velocityEstimator;
     public LayerMask sliceableLayer;
-
     public Material crossSectionMaterial;
 
     public float cutForce = 2;
-        
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float minCutVelocity = 5; 
 
-    // Update is called once per frame
     void FixedUpdate()
-    {
+    { .
         bool hasHit = Physics.Linecast(startSlicePoint.position, endSlicePoint.position, out RaycastHit hit, sliceableLayer);
         if (hasHit)
         {
             GameObject target = hit.transform.gameObject;
             Slice(target);
-        } 
+        }
     }
 
     public void Slice(GameObject target)
     {
         Vector3 velocity = velocityEstimator.GetVelocityEstimate();
-        Vector3 planeNormal = Vector3.Cross(endSlicePoint.position - startSlicePoint.position, velocity);
-        planeNormal.Normalize();
-        
-        SlicedHull hull = target.Slice(endSlicePoint.position, planeNormal);
-
-        if (hull != null)
+        if (velocity.magnitude >= minCutVelocity)
         {
-            GameObject upperHull = hull.CreateUpperHull(target,crossSectionMaterial);
-            SetupSlicedComponent(upperHull);
-            upperHull.layer = target.layer;
-            GameObject lowerHull = hull.CreateLowerHull(target,crossSectionMaterial);
-            SetupSlicedComponent(lowerHull);
-            lowerHull.layer = target.layer;
+            Vector3 planeNormal = Vector3.Cross(endSlicePoint.position - startSlicePoint.position, velocity);
+            planeNormal.Normalize();
+        
+            SlicedHull hull = target.Slice(endSlicePoint.position, planeNormal);
+            if (hull != null)
+            {
+                GameObject upperHull = hull.CreateUpperHull(target, crossSectionMaterial);
+                SetupSlicedComponent(upperHull);
+                upperHull.layer = target.layer;
+
+                GameObject lowerHull = hull.CreateLowerHull(target, crossSectionMaterial);
+                SetupSlicedComponent(lowerHull);
+                lowerHull.layer = target.layer;
             
-            Destroy(target);
+                Destroy(target);
+            }
         }
     }
 

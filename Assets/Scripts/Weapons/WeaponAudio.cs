43afd67e088ee7fdcc00b1821b8c162cc18e161c
Anakaryna <78374876+Anakaryna@users.noreply.github.com>
2024-04-18 10:39:11 +0200

    Fix Weapon audio

--- Assets/Scripts/Weapons/WeaponAudio.cs
+++ Assets/Scripts/Weapons/WeaponAudio.cs
@@ -3,14 +3,13 @@ using UnityEngine;
 public class WeaponAudio : MonoBehaviour
 {
     public AudioSource audioSource;
-    public AudioClip impactSound;
     public string destructibleTag = "Destructible";
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag(destructibleTag))
         {
-            audioSource.PlayOneShot(impactSound);
+            audioSource.Play();
         }
     }
 }
